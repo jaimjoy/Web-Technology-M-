2ae5bf6edb28b1b402d5318955c3c61303bc8add
AbdullaSina <ibnasina98@gmail.com>
2026-05-17 23:02:02 +0600

    Create_article function included

--- blog-and-news-publishing-platform/app/views/author/create_article.php
+++ blog-and-news-publishing-platform/app/views/author/create_article.php
@@ -2,3 +2,31 @@
 require_once("../../middleware/author.php");
 require_once("../../models/Article.php");
 
+$article = new Article();
+
+if(isset($_POST["create"]))
+{
+    $title = trim($_POST["title"]);
+    $body = trim($_POST["body"]);
+    $excerpt = trim($_POST["excerpt"]);
+    $category_id = $_POST["category_id"];
+    $status = $_POST["status"];
+
+    if(!empty($title) &&!empty($body))
+    {
+        $article->createArticle(
+            $_SESSION["user_id"],
+            $category_id,
+            $title,
+            $body,
+            $excerpt,
+            $status
+        );
+        echo "Article Submitted For Review!";
+    }
+}
+
+$categories = $article->getCategories();
+
+?>
+
