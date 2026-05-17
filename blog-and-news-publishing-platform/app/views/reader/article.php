ee66ce6e718dfba8fc34030bd30fb9160faa37f7
HABIBA NASRIN SUMAYA <23-50339-1@student.aiub.edu>
2026-05-17 22:35:20 +0600

    article view base

---
+++ blog-and-news-publishing-platform/app/views/reader/article.php
@@ -0,0 +1,72 @@
+﻿<?php
+require_once("../../models/Article.php");
+session_start();
+
+$article = new Article();
+
+if(isset($_GET["id"]))
+{
+    $id = $_GET["id"];
+    $article->increaseViewCount($id);
+    if(isset($_SESSION["user_id"]))
+    {
+        $article->saveReadingHistory(
+            $id,
+            $_SESSION["user_id"]
+        );
+    }
+
+    $row = $article->getArticleById($id);
+}
+else
+{
+    die("Article ID Missing");
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Article Details</title>
+    <link
+        rel="stylesheet"
+        href="../../../assets/css/style.css"
+    >
+</head>
+<body>
+    <div class="container">
+        <a
+            href="home.php"
+            class="dashboard-card"
+            style="display:inline-block;margin-bottom:20px;"
+            >
+                ← Back To Home
+        </a>
+
+    <?php
+
+    if($row)
+    {
+        echo "<div class='card'>";
+
+        echo "<h1>";
+        echo $row["title"];
+        echo "</h1>";
+
+        echo "<b>Author:</b> ";
+        echo $row["author_name"];
+
+        echo "<br><br>";
+
+    }
+    else
+    {
+        echo "Article Not Found";
+    }
+
+    ?>
+
+    </div>
+</body>
+</html>
