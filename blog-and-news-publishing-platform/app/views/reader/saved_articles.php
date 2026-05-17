1238491d293df6fbde2b8abf9a3afc618d47e69b
HABIBA NASRIN SUMAYA <23-50339-1@student.aiub.edu>
2026-05-17 22:36:57 +0600

    saved articles list

---
+++ blog-and-news-publishing-platform/app/views/reader/saved_articles.php
@@ -0,0 +1,57 @@
+﻿<?php
+require_once("../../models/Article.php");
+session_start();
+
+$article = new Article();
+
+$result = $article->getSavedArticles(
+    $_SESSION["user_id"]
+);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Saved Articles</title>
+    <link
+        rel="stylesheet"
+        href="../../../assets/css/style.css"
+    >
+</head>
+<body>
+    <div class="container">
+        <a
+        href="dashboard.php"
+        class="dashboard-card"
+        style="display:inline-block;margin-bottom:20px;"
+        >
+            ← Back To Dashboard
+        </a>
+
+    <h2>Saved Articles</h2>
+
+    <?php
+    if($result->num_rows > 0)
+    {
+        while($row = $result->fetch_assoc())
+        {
+            echo "<div class='card'>";
+
+            echo "<h3>";
+
+            echo $row["title"];
+
+            echo "</h3>";
+
+            echo "</div>";
+        }
+    }
+    else
+    {
+        echo "No Saved Articles";
+    }
+    ?>
+    </div>
+</body>
+</html>
