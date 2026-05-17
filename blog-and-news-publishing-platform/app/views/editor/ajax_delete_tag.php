ecc039f8e87ebbfc628fc80ac2a42d9fb1c7dc4f
jaimjoy <jaim81200@gmail.com>
2026-05-17 20:55:53 +0600

    AJAX for delete Tag is fully implemented

--- blog-and-news-publishing-platform/app/views/editor/ajax_delete_tag.php
+++ blog-and-news-publishing-platform/app/views/editor/ajax_delete_tag.php
@@ -10,5 +10,14 @@ if(isset($_POST["id"]))
 
     $sql="DELETE FROM tags
     WHERE id=?";
+
+    $stmt=$conn->prepare($sql);
+
+    $stmt->bind_param(
+        "i",
+        $id
+    );
+
+    $stmt->execute();
 }
 ?>
