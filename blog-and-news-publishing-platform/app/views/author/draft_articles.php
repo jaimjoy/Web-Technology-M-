35724666b64ee7bc2323f3cfbef686544dfc2931
AbdullaSina <ibnasina98@gmail.com>
2026-05-17 23:12:49 +0600

    draft_article html body included

--- blog-and-news-publishing-platform/app/views/author/draft_articles.php
+++ blog-and-news-publishing-platform/app/views/author/draft_articles.php
@@ -9,3 +9,89 @@ $result = $article->getDraftArticles(
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Draft Articles</title>
+    <link
+        rel="stylesheet"
+        href="../../../assets/css/style.css"
+    >
+</head>
+<body>
+    <div class="container">
+        <a
+        href="dashboard.php"
+        class="dashboard-card"
+        style="display:inline-block;margin-bottom:20px;"
+        >
+            ← Back To Dashboard
+        </a>
+
+    <h1>Draft Articles</h1>
+    <?php
+    if($result->num_rows > 0)
+    {
+        while($row = $result->fetch_assoc())
+        {
+            echo "<div class='card'>";
+
+            echo "<h3>";
+
+            echo $row["title"];
+
+            echo "</h3>";
+
+            echo "<b>Status:</b> ";
+
+            echo $row["status"];
+
+            echo "<br><br>";
+            echo "</div>";
+
+            echo "<a href='edit_article.php?id=";
+
+            echo $row["id"];
+
+            echo "'>";
+
+            echo "<button>Edit</button>";
+
+            echo "</a>";
+
+            echo " | ";
+
+            echo "<a href='delete_article.php?id=";
+
+            echo $row["id"];
+
+            echo "'>";
+
+            echo "<button class='btn-danger'>Delete</button>";
+
+            echo " | ";
+
+            echo "<a href='submit_review.php?id=";
+
+            echo $row["id"];
+
+            echo "'>";
+
+            echo "<button class='btn-success'>Submit For Review</button>";
+
+            echo "</a>";
+
+            echo "</a>";
+
+            echo "<br><br>";
+        }
+    }
+    else
+    {
+        echo "No Draft Articles";
+    }
+    ?>
+    </div>
+</body>
+</html>
+
