67e9e371011bcee6b2775bc77e2f17f93f84e11c
AbdullaSina <ibnasina98@gmail.com>
2026-05-17 22:46:32 +0600

    Header included

--- blog-and-news-publishing-platform/app/views/author/ajax_delete_article.php
+++ blog-and-news-publishing-platform/app/views/author/ajax_delete_article.php
@@ -1,31 +1,4 @@
 <?php
 require_once("../../middleware/author.php");
 require_once(__DIR__ . "/../../../config/database.php");
-
-global $conn;
-
-if(isset($_POST["id"]))
-{
-    $id=$_POST["id"];
-
-    $sql="DELETE FROM articles
-    WHERE id=?
-    AND author_id=?";
-
-    $stmt=$conn->prepare($sql);
-
-    $stmt->bind_param(
-        "ii",
-        $id,
-        $_SESSION["user_id"]
-    );
-
-    $stmt->execute();
-
-    header("Content-Type: application/json");
-    echo json_encode([
-        "status"=>"success",
-        "message"=>"Article Deleted"
-    ]);
-}
 ?>
